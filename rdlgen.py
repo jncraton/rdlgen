b161bbc55e0576c86288696ff06ed05451874109
Sam Rudloff <srudloff@ingham.org>
2021-10-11 11:34:57 -0400

    Update rdlgen.py

--- rdlgen.py
+++ rdlgen.py
@@ -1,24 +1,30 @@
+"""Generate Rdl File using Jinga2 Templating."""
+
 from jinja2 import Template
 import codecs
 import re
 import string
 import random
 
-class RDL():
-    
-    def __init__(self,server_url,datasource,query,template='template.rdl'):
+
+class RDL:
+    """Rdl Generator Class."""
+
+    def __init__(self, server_url, datasource, query, template='template.rdl'):
+        """Constructor."""
         self.query = query
         self.server_url = server_url
         self.datasource = datasource
         self.template = template
-        
+
         self.fields = []
         self.paramters = []
 
         self.init_parameters()
         self.init_fields()
-        
-    def get_text(self,):
+
+    def get_text(self):
+        """Render Template."""
         with open(self.template, 'r') as template_file:
             template = Template(template_file.read().decode('utf8'))
             return template.render(
@@ -27,55 +33,65 @@ class RDL():
                 data_source_id=self.gen_id(),
                 fields=self.fields,
                 parameters=self.parameters,
-                server_url = self.server_url,
-                report_id = self.gen_id(),
+                server_url=self.server_url,
+                report_id=self.gen_id(),
             )
-    
-    def write_file(self,filename):
-            with open(filename, 'w') as out_file:
-                out_file.write(codecs.BOM_UTF8)
 
-                out_file.write(self.get_text())
-    
+    def write_file(self, filename):
+        """Write Templated File."""
+        with open(filename, 'w') as out_file:
+            out_file.write(codecs.BOM_UTF8)
+
+            out_file.write(self.get_text())
+
     def init_fields(self):
-        m = re.match(r'select(.*?)\nfrom', self.query, flags=re.IGNORECASE|re.MULTILINE|re.DOTALL)
-        
+        """Init fields."""
+        m = re.match(
+            r'select(.*?)\nfrom',
+            self.query,
+            flags=re.IGNORECASE | re.MULTILINE | re.DOTALL,
+        )
+
         for field in m.group(1).strip().split(',\n'):
             names = field.split(' as ')
-            
+
             name = names[0]
-            
+
             if len(names) > 1:
                 name = names[1]
-            
-            name = name.replace('\'','')
+
+            name = name.replace('\'', '')
             name = name.strip()
-            
-            self.fields.append({
-                "name":name
-            })
-  
+
+            self.fields.append({"name": name})
+
     def init_parameters(self):
+        """Init Parameters."""
         matches = re.findall(r'@[a-zA-Z0-9_]*?[ \n\r]', self.query, flags=re.MULTILINE)
 
-        self.parameters = set([m.strip().replace('@','') for m in matches])
-            
-    def gen_id(self,size=16,chars=string.ascii_uppercase + string.digits):
+        self.parameters = set([m.strip().replace('@', '') for m in matches])
+
+    def gen_id(self, size=16, chars=string.ascii_uppercase + string.digits):
+        """Generate Id."""
         return ''.join(random.choice(chars) for _ in range(size))
+
 
 if __name__ == '__main__':
     import sys
-    
+
     try:
         server_url = str(sys.argv[1])
         datasource = '/' + str(sys.argv[2])
         outfile = str(sys.argv[3])
         query = str(sys.stdin.read())
     except:
-        print 'Usage: %s [server_url] [datasource] [outfile]\nSQL query is supplied on stdin' % (sys.argv[0],)
+        print(
+            'Usage: %s [server_url] [datasource] [outfile]\nSQL query is supplied on'
+            ' stdin' % (sys.argv[0],)
+        )
         exit(1)
-    
-    rdl = RDL(server_url,datasource,query)
+
+    rdl = RDL(server_url, datasource, query)
 
     rdl.write_file(outfile)
-    print "Output written to %s" % outfile
+    print("Output written to %s" % outfile)
